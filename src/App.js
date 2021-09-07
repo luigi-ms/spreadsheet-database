d4e1ab81e38b1590ebf3530c17ad5439349ad2d4
Luigi Moraes <luigimoraes3@gmail.com>
2021-09-07 14:42:07 -0300

    design update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -7,10 +8,13 @@ import DataBase from './DataBase.js';
 import PlayArrow from '@material-ui/icons/PlayArrow';
 
 const useStyles = makeStyles(theme => ({
-	title: { textAlign: "center" }
+	title: { 
+		marginTop: "3vh",
+		marginBottom: "3vh"
+	}
 }));
 
-function App() {
+export default function App() {
 	const classes = useStyles();
 
   return (
@@ -18,22 +22,18 @@ function App() {
 			<Grid container 
 				justifyContent="center" 
 				spacing={2}>
-				<Grid item alignItems="center" xs={10}>
-					<h1 className={classes.title}>SpreadSheet Database</h1>
-				</Grid>
-				<Grid item xs={6}>
-					<Button variant="contained" 
-						color="primary">Limpar tabela</Button>
+				<Grid item xs={10} className={classes.title}>
+					<Typography variant="h5" align="center">SpreadSheet Database</Typography>
 				</Grid>
 				<Grid item xs={8}>
 					<TextField type="search" 
 						label="Insira um comando SQL" 
-						variant="outlined" 
+						variant="filled" 
 						fullWidth/>
 				</Grid>
 				<Grid item xs={2}>
 					<Button variant="contained" 
-						color="secondary"><PlayArrow/></Button>
+						color="primary"><PlayArrow/></Button>
 				</Grid>
 				<Grid item xs={11}>
 					<DataBase/>
@@ -42,5 +42,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
--- src/DataBase.js
+++ src/DataBase.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -8,22 +9,36 @@ import TableHead from '@material-ui/core/TableHead';
 
 let defaultColumnNames = ["column01", "column02", "column03", "column04", "column05"];
 
+const useStyles = makeStyles(theme => ({
+	tableHeadColors: {
+		backgroundColor: "rgb(16, 29, 37)",
+		color: "rgb(210, 215, 219)"
+	},
+	tableBodyColors:{
+		backgroundColor: "rgb(35, 45, 54)",
+		color: "rgb(173, 181, 184)"
+	},
+	tableSpacing: { marginTop: "3vh" }
+}));
+
 export default function DataBase(){
+	const classes = useStyles();
+
 	return(
-		<TableContainer>
+		<TableContainer className={classes.tableSpacing}>
 			<Table aria-label="Spreadsheet" color="primary">
 				<TableHead>
-					<TableRow>
+					<TableRow align="right">
 						{defaultColumnNames.map(name => (
-							<TableCell>{name}</TableCell>
+							<TableCell className={classes.tableHeadColors}>{name}</TableCell>
 						))}
 					</TableRow>
 				</TableHead>
 				<TableBody>
-					{[1, 2, 3, 4, 5].map(column => (
-						<TableRow>
+					{[0, 1, 2, 3, 4].map(column => (
+						<TableRow align="right">
 							{[0, 1, 2, 3, 4].map(row => (
-								<TableCell></TableCell>	
+								<TableCell className={classes.tableBodyColors}>{column.toString()+row}</TableCell>	
 							))}
 						</TableRow>
 					))}
